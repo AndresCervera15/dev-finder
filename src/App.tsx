9366be74f2b221927aa6d3d60b8d20dbdc076950
AndresCervera15 <andres98100@gmail.com>
2023-11-19 15:48:19 -0600

    chore: theme button

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Search } from "./components/Search";
 import { UserCard } from "./components/UserCard";
+import { Icon } from "@iconify/react";
 function App() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState(null);
   const [isFirstInput, setIsFirstInput] = useState(true);
+  const [isLightMode, setIsLightMode] = useState(true);
+
   useEffect(() => {
     const fetchData = async (search: string) => {
       if (search) {
@@ -21,6 +24,37 @@ function App() {
     <div className="app">
       <header>
         <h1>devFinder</h1>
+        {isLightMode ? (
+          <button
+            className="themeButton"
+            onClick={() => setIsLightMode(!isLightMode)}
+          >
+            DARK
+            <Icon
+              style={{
+                marginLeft: "8px",
+              }}
+              icon="ic:baseline-dark-mode"
+              color="#ffffff"
+              width="32"
+            />
+          </button>
+        ) : (
+          <button
+            className="themeButton"
+            onClick={() => setIsLightMode(!isLightMode)}
+          >
+            LIGTH
+            <Icon
+              style={{
+                marginLeft: "8px",
+              }}
+              icon="material-symbols-light:light-mode-rounded"
+              color="white"
+              width="32"
+            />
+          </button>
+        )}
       </header>
       <main>
         <Search setSearch={setSearch} />
